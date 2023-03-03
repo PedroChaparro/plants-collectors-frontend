c25f1f0c5e94e6a0a67bd3e47e037eb2e3ba9fef
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2023-03-03 18:58:16 -0500

    feat: 🔐 Add environment variables support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(); // Load .enf file from the default file
   runApp(const MyApp());
 }
 
